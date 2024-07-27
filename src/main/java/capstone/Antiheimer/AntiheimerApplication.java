2d9034d7543f389cea359b48aa07bfdd37ffadbb
yereumi <lovelyyeji01@gmail.com>
2024-07-27 18:48:30 +0900

    test

---
+++ src/main/java/capstone/Antiheimer/AntiheimerApplication.java
@@ -0,0 +1,13 @@
+package capstone.Antiheimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiheimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntiheimerApplication.class, args);
+	}
+
+}
